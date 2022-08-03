104f6d5fa406d671154d9cfe741163cfb7f4babf
healthedata1 <ehaas@healthedata1.com>
2022-08-02 23:09:05 -0700

    updated bash script to allow sushi in ig-pub

--- push_to_autobuild.sh
+++ push_to_autobuild.sh
@@ -2,8 +2,17 @@
 # exit when any command fails
 set -e
 
+while getopts s option
+do
+ case "${option}"
+ in
+ s) SUSHI=1;;
+ esac
+done
+
 echo "================================================================="
 echo "=== commit and load to github for autopublisher ==="
+echo "-s parameter for enabling sushi in ig-publisher = $SUSHI"
 echo "================================================================="
 
 echo "================================================================="
@@ -13,11 +22,14 @@ echo "================================================================="
 ls docs/*.md && rm docs/*.md
 ls docs/*.md && echo 'done!'
 
+
+if ! [[ $SUSHI ]]; then
 echo "================================================================="
 echo "=== rename the 'input/fsh' folder to 'input/_fsh'  ==="
 echo "================================================================="
 trap "echo '=== rename the input/_fsh folder to input/fsh  ==='; mv input/_fsh input/fsh" EXIT
 [[ -d input/fsh ]] && mv input/fsh input/_fsh
+fi
 
 git status
 
