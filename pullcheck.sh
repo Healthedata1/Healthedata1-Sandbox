b5acd4903d90ae4cf17b2ee72f69d37e0dd2c2c9
healthedata1 <ehaas@healthedata1.com>
2026-01-07 20:21:41 -0800

    update pullcheck script to call json updates

--- pullcheck.sh
+++ pullcheck.sh
@@ -2,27 +2,41 @@
 # exit when any command fails
 set -e
 
-
-echo "================================================================="
-echo "=== check which files changes in remote master before pulling ==="
-echo "================================================================="
-echo "=== git fetch ==="
-echo "=== git diff --name-status ...origin ==="
-echo "=== -r parameter for checking only yaml-generated files = $RFILES ==="
-echo "=== git diff --name-status ...origin -- input/resources -- input/examples ==="
-echo "================================================================="
+echo ""
+echo "=========================================================================="
+echo "=== check which files changes and the status code in remote master before pulling ============"
+echo ""
+echo "======== Status Codes =============="
+echo "====  modify="M"      # Modified  ===="
+echo "====  add="A"         # Added     ===="
+echo "====  delete="D"      # Deleted   ===="
+echo "====  rename="R"      # Renamed   ===="
+echo "====  copy="C"        # Copied   ===="
+echo "====  unmerged="U"    # Unmerged (conflict)  ===="
+echo "====  typechange="T"  # Type changed (file ↔ symlink)  ===="
+echo ""
+echo " === Alert when an input/resources/*.json file was edited directly ======="
+echo " =========== and the corresponding YAML source file not updated ========= "
+echo "=========================================================================="
+echo ""
 
 git fetch
-# git diff --name-status ...origin
-changed_files=$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)
-
-for file in $changed_files; do
-
-if file == input/resources/*.json then
-    if ! echo "$changed_files" | grep -q "^${file%.json}.yaml"; then
-        echo "⚠️  $file was edited directly! convert to YAML and overwrite the YAML in resources-yaml"
-    else echo "changed file = $file"
-    fi
+changed_files=$(git diff --name-status ...origin)
+# changed_files=$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)
+# Git status codes
+statuses="M A D R C U T"
+for file in $changed_files; do  #The unquoted $changed_files triggers word splitting,
+  if [[ " $statuses " =~ " $file " ]]; then
+    status_code=$file
+  elif [[ $file == input/resources/*.json ]]; then
+      if ! echo "$changed_files" | grep -q "^${file%.json}.yaml"; then
+          echo ""
+          echo "⚠️  $file was edited directly! (status code = $status_code)"
+          echo "    ℹ️  to reconcile with the YAML file pull and overwrite the YAML in resources-yaml"
+          echo ""
+      fi
+  else echo "ℹ️  changed file = $file (status code = $status_code)"
+  fi
 done
 
 
